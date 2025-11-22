4cb9558b7f60a376cb3d1727872cb4140144dcca
amaan-exe <amanullah2607main@gmail.com>
2025-11-23 01:48:29 +0530

    updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -42,7 +42,7 @@ const Header = () => {
                     <Link to="/" className="flex items-center space-x-3 group">
                         <div className="group-hover:scale-110 transition-transform duration-300">
                             <img
-                                src="/src/assets/logo.jpg"
+                                src="/src/assets/gallery/logo.jpg"
                                 alt="Smart Construction & Developers Logo"
                                 className="w-12 h-12 object-contain"
                             />
